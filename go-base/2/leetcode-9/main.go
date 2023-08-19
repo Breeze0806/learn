a91f9767f2a184703ea91c995015289dfffcde29
Breeze0806 <fhzxfxd@126.com>
2023-08-20 00:07:36 +0800

    feat: 新增题解

---
+++ go-base/2/leetcode-9/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(isPalindrome(math.MaxInt32))
+}
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	x64 := x //整形
+	px64 := 0
+	for x64 != 0 {
+		px64 = px64*10 + x64%10
+		x64 /= 10
+	}
+	fmt.Println(px64)
+	return px64 == x //布尔型
+}
+
+func isPalindromeInt32(x int) bool {
+	if x < 0 {
+		return false
+	}
+
+	x32 := int32(x)
+	px32 := int32(x)
+	for x32 != 0 {
+		px32 = px32*10 + x32%10
+		x32 /= 10
+	}
+	return px32 == int32(x)
+}
+
+func isPalindromeInt64(x int) bool {
+	if x < 0 {
+		return false
+	}
+	x64 := int64(x)
+	px64 := int64(0)
+	for x64 != 0 {
+		px64 = px64*10 + x64%10
+		x64 /= 10
+	}
+	return px64 == int64(x)
+}
